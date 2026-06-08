504ac894a2da113aa8bebfc9fcb28a7cccedd718
Nasereddine Lakhal <naserlakhal@gmail.com>
2026-06-08 21:31:44 +0100

    begin with auth url endpoint

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,8 +1,6 @@
-export const API_BASE_URL = "/api/";
-
 export const API_AUTH = {
-  LOGIN: "",
-  SIGNUP: "",
+  LOGIN: `${import.meta.env.VITE_API_BASE_URL}${import.meta.env.VITE_API_BASE_URL}/auth/login`,
+  SIGNUP: `${import.meta.env.VITE_API_BASE_URL}${import.meta.env.VITE_API_BASE_URL}/auth/register`,
 };
 
 export const Outfit = {};
