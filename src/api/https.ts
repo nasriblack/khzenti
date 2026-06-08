b715eb18fedb0c437f4504108b426139da93ed00
Nasereddine Lakhal <naserlakhal@gmail.com>
2026-06-08 21:17:55 +0100

    add https file

---
+++ src/api/https.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+// import { attachInterceptors } from "./interceptors";
+
+export const http = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 30_000,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+// attachInterceptors(http);
