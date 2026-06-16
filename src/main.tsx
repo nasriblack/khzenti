a73e77cebf226b083b4451274642a27c9a094b69
Nasereddine Lakhal <naserlakhal@gmail.com>
2026-06-16 19:09:16 +0100

    revert khzenti to the main page

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,15 @@ import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import KhzantiLanding from "./pages/LandingPage.tsx";
+// import KhzantiLanding from "./pages/LandingPage.tsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <KhzantiLanding />
-      {/* <App /> */}
+      {/* <KhzantiLanding /> */}
+      <App />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
